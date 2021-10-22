111168ab276a7693a0dd4bf312b9a268a71ff249
Leandran <leandranm@outlook.com>
2021-10-22 23:34:21 +0200

    updated views and model validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
-    validates :url, presence: true
+    #urls can't be blank and only allowed for Wikipedia urls
+
+    validates :url, presence: true, uniqueness: true
+    validates :url, format: URI::regexp(%w[http https])
+    validates :is_wikipedia_url
+
+    def is_wikipedia_url
+        uri = URI.parse(url.downcase)
+        if uri.host
+            return true if uri.host.match /[a-z]{2}\.wikipedia\.org/
+            errors.add(:url, "must be an article on wikipedia.org")
+        end
+    end
 end
