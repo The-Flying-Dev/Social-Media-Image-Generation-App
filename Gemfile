5458137b2d848547a69625ca92151149a86c5887
Leandran <leandranm@outlook.com>
2021-10-23 01:07:31 +0200

    added bannerbear api with json payload

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "slim-rails"
 gem "bulma"
 gem "httparty"
 gem "nokogiri"
+gem "figaro"
 
 
 # Use Redis adapter to run Action Cable in production
--- app/models/article.rb
+++ app/models/article.rb
@@ -32,5 +32,33 @@ class Article < ApplicationRecord
     def first_sentence
       Nokogiri::HTML.parse(self.html).at('.mw-parser-output > p:not(.mw-empty-elt)').text.split(".").first.gsub(/\(.*\)/, "").gsub(" ,",",")
     end
+
+    #JSON payload to POST to Bannerbear
+    def post_to_bannerbear
+      return if !self.html
+      payload = {
+        "template": ENV['bannerbear_template_id'],
+        "modifications": [
+          {
+            "name": "image",
+            "image_url": self.image
+          },
+          {
+            "name": "intro",
+            "text": self.first_sentence
+          },
+          {
+            "name": "title",
+            "text": self.title
+          }
+        ]
+      }
+      response = HTTParty.post("https://api.bannerbear.com/v2/images",{
+        body: payload,
+        headers: {"Authorization" => "Bearer #{ENV['bannerbear_api_key']}"}
+      })
+    end
+
+    after_commit :post_to_bannerbear
     
 end
